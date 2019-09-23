69efa751c102d6c53a6824b046fce71d2b525720
Andres Gonzales <andresgonzales@Andress-MacBook-Air.local>
2019-09-22 18:34:39 -0700

    finished flix2

---
+++ flixApp/flixApp/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixApp
+//
+//  Created by Andres Gonzales on 9/22/19.
+//  Copyright © 2019 Andres Gonzales. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
--- flixApp/flixApp/MoviesViewController.swift
+++ flixApp/flixApp/MoviesViewController.swift
@@ -60,6 +60,8 @@ class MoviesViewController: UIViewController,UITableViewDelegate,UITableViewData
         
         cell.sysnopsisLabel.text = synopsis
         cell.titleLabel.text  = title
+        cell.sysnopsisLabel.sizeToFit()
+        cell.titleLabel.sizeToFit()
         
         let baseUrl = "https://image.tmdb.org/t/p/w185"
         let posterPath = movie["poster_path"] as! String
@@ -70,14 +72,24 @@ class MoviesViewController: UIViewController,UITableViewDelegate,UITableViewData
         return cell;
     }
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        let cell = sender as! UITableViewCell
+        
+        let indexPath = tableView.indexPath(for: cell)!
+        
+        let movie = movies[indexPath.row]
+     
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        
+        detailsViewController.movie = movie
+        tableView.deselectRow(at: indexPath, animated: true)
     }
-    */
+    
 
 }
